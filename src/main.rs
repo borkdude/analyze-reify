5c8b39bbaf9b49f9131419266cab7bf2b45fdc77
Michiel Borkent <michielborkent@gmail.com>
2020-03-14 17:20:23 +0100

    Process files async

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use tree_sitter::{Parser, Language, Node};
-use std::path::{Path};
+use std::path::{Path, PathBuf};
 use std::env;
 use glob::glob;
 use std::fs::metadata;
+use rayon::prelude::*;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 extern "C" { fn tree_sitter_clojure() -> Language; }
 
@@ -34,7 +36,11 @@ fn print_reify_usage_from_node(node: Node, bytes: &[u8]) {
     }
 }
 
-fn print_reify_usage_from_file_path(parser: &mut Parser, path: &Path) {
+fn print_reify_usage_from_file_path(path: &Path) {
+    let language: Language = unsafe { tree_sitter_clojure() };
+    let mut parser = Parser::new();
+    parser.set_language(language).unwrap();
+
     let contents = std::fs::read_to_string(path).unwrap();
     let bytes = contents.as_bytes();
     let tree = parser.parse(&bytes, None).unwrap();
@@ -54,21 +60,22 @@ fn paths_from_arg(arg: &String) -> glob::Paths {
 }
 
 fn main() {
-    let language = unsafe { tree_sitter_clojure() };
-    let mut parser = Parser::new();
-    parser.set_language(language).unwrap();
-
-    for arg in env::args().skip(1) {
-        for entry in paths_from_arg(&arg) {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let atomic_counter = AtomicUsize::new(0);
+    args.into_par_iter().for_each(|arg| {
+        let paths: Vec<Result<PathBuf,_>> = paths_from_arg(&arg).collect();
+        paths.into_par_iter().for_each(|entry| {
             match entry {
                 Ok(path) => {
                     let path = path.as_path();
                     if path.is_file() {
-                        print_reify_usage_from_file_path(&mut parser, path);
+                        atomic_counter.fetch_add(1, Ordering::Relaxed);
+                        print_reify_usage_from_file_path(path);
                     }
                 },
-                Err(e) => panic!(e)
+                Err(e) => panic!(format!("Unexpected error while analyzing {}", e))
             }
-        }
-    }
+        });
+    });
+    eprintln!("\n=== Processed {} files ===", atomic_counter.load(Ordering::SeqCst))
 }
